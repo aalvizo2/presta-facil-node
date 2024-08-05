4c27b5a5250fc27e6b13f05f94e1dc5b7a41d7fd
Alan Alvizo <aalvizo24@hotmail.com>
2024-08-05 08:59:20 -0600

    mejoras para que no se apague el servidor

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const login = require('./routes/login')
+const login = require('./routes/login');
+const credito = require('./routes/credito');
+const upload = require('./routes/upload');
+const plazo = require('./routes/plazo');
+const path = require('path');
+const estadoCuenta = require('./routes/estado-cuenta');
+const movimientos = require('./routes/movimientos');
+const perfiles = require('./routes/perfiles');
+const cobranza = require('./routes/cobranza');
+const gastos = require('./routes/gastos');
+
 const app = express();
-const credito= require('./routes/credito')
-const upload= require('./routes/upload')
-const plazo= require('./routes/plazo')
-const path= require('path')
-const estadoCuenta= require('./routes/estado-cuenta')
-const movimientos= require('./routes/movimientos')
-const perfiles= require('./routes/perfiles')
-const cobranza= require('./routes/cobranza')
-const gastos= require('./routes/gastos')
 
 // Configuración de middlewares
 app.use(bodyParser.json());
 //setting up /cedula folder
-app.use('/cedula', express.static(path.join(__dirname, 'cedula')))
+app.use('/cedula', express.static(path.join(__dirname, 'cedula')));
 // Configuración de CORS
 const corsOptions = {
   origin: ['https://aalvizo2.github.io', 'http://localhost:3000', 'http://192.168.1.75:3000'],
@@ -26,26 +27,36 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
-
 // Definición de rutas
-app.use('/', login)
-app.use('/', credito)
-app.use('/', upload)
-app.use('/', plazo)
-app.use('/', estadoCuenta)
-app.use('/', movimientos)
-app.use('/', perfiles)
-app.use('/', cobranza)
-app.use('/', gastos)
+app.use('/', login);
+app.use('/', credito);
+app.use('/', upload);
+app.use('/', plazo);
+app.use('/', estadoCuenta);
+app.use('/', movimientos);
+app.use('/', perfiles);
+app.use('/', cobranza);
+app.use('/', gastos);
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
-})
+});
+
+// Ruta para evitar que el servidor se duerma en Render
+app.get('/keep-alive', (req, res) => {
+  res.send('Server is alive');
+});
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
-})
+});
 
+// Mantener el servidor activo en Render
+setInterval(() => {
+  fetch(`http://localhost:${PORT}/keep-alive`)
+    .then(res => res.text())
+    .then(res => console.log(res))
+    .catch(err => console.log('Error keeping alive:', err));
+}, 5 * 60 * 1000); // Cada 5 minutos
