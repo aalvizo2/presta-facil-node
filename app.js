dd4a852c4672bef5305aaa03de2fabb2e1377527
Alan Alvizo <aalvizo24@hotmail.com>
2024-08-14 17:51:35 -0600

    Revert "cambios en database"
    This reverts commit eb18acc07b1e5d7ac60e6b79944f390d4e54bc25.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+<<<<<<< HEAD
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
@@ -13,11 +14,28 @@ const movimientos= require('./routes/movimientos')
 const perfiles= require('./routes/perfiles')
 const cobranza= require('./routes/cobranza')
 const gastos= require('./routes/gastos')
+=======
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const login = require('./routes/login');
+const credito = require('./routes/credito');
+const upload = require('./routes/upload');
+const plazo = require('./routes/plazo');
+const path = require('path');
+const estadoCuenta = require('./routes/estado-cuenta');
+const movimientos = require('./routes/movimientos');
+const perfiles = require('./routes/perfiles');
+const cobranza = require('./routes/cobranza');
+const gastos = require('./routes/gastos');
+
+const app = express();
+>>>>>>> parent of eb18acc0 (cambios en database)
 
 // Configuración de middlewares
 app.use(bodyParser.json());
 //setting up /cedula folder
-app.use('/cedula', express.static(path.join(__dirname, 'cedula')))
+app.use('/cedula', express.static(path.join(__dirname, 'cedula')));
 // Configuración de CORS
 const corsOptions = {
   origin: ['https://aalvizo2.github.io', 'http://localhost:3000', 'http://192.168.1.75:3000'],
@@ -25,26 +43,50 @@ const corsOptions = {
   credentials: true
 };
 app.use(cors(corsOptions));
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> parent of eb18acc0 (cambios en database)
 
 // Definición de rutas
-app.use('/', login)
-app.use('/', credito)
-app.use('/', upload)
-app.use('/', plazo)
-app.use('/', estadoCuenta)
-app.use('/', movimientos)
-app.use('/', perfiles)
-app.use('/', cobranza)
-app.use('/', gastos)
+app.use('/', login);
+app.use('/', credito);
+app.use('/', upload);
+app.use('/', plazo);
+app.use('/', estadoCuenta);
+app.use('/', movimientos);
+app.use('/', perfiles);
+app.use('/', cobranza);
+app.use('/', gastos);
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
+<<<<<<< HEAD
 })
+=======
+});
+
+// Ruta para evitar que el servidor se duerma en Render
+app.get('/keep-alive', (req, res) => {
+  res.send('Server is alive');
+});
+>>>>>>> parent of eb18acc0 (cambios en database)
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+<<<<<<< HEAD
 })
+=======
+});
+
+// Mantener el servidor activo en Render
+setInterval(() => {
+  fetch(`http://localhost:${PORT}/keep-alive`)
+    .then(res => res.text())
+    .then(res => console.log(res))
+    .catch(err => console.log('Error keeping alive:', err));
+}, 5 * 60 * 1000); // Cada 5 minutos
+>>>>>>> parent of eb18acc0 (cambios en database)
